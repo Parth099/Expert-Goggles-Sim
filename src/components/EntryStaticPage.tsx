64b68f5cf9b36195f496944eac433c7783260ef8
Parth099 <parthnpatel715@gmail.com>
2022-08-03 13:06:50 -0400

    created static page

---
+++ src/components/EntryStaticPage.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function EntryStaticPage() {
+    return <div>EntryStaticPage</div>;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "./App";
+import EntryStaticPage from "./components/EntryStaticPage";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<EntryStaticPage />}></Route>
+                <Route path="/:url" element={<App />}></Route>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
+);
